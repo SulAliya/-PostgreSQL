a16597842a4ecb5416397e6970de263054cffeb4
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-05-04 19:15:10 +0500

    Заполнили таблицу данными из json-файла

--- conn_bd.py
+++ conn_bd.py
@@ -1,24 +1,23 @@
-import csv
+import json
 import psycopg2
 
-
 with psycopg2.connect(
-    host='Localhost',
-    database='HH_Vacancies',
-    user='postgres',
-    password='Qwerty098'
+        host='localhost',
+        database='HH_Vacancies',
+        user='postgres',
+        password='Qwerty098'
 ) as conn:
     with conn.cursor() as cur:
-        with open('vacancy_json.json', encoding="utf8") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                cur.execute('INSERT INTO vacancy_table VALUES (%s,%s,%s,%s,%s,%s)', (row.get('company'),
-                                                                                  row.get('job_title'),
-                                                                                  row.get('link_to_vacancy'),
-                                                                                  row.get('salary'),
-                                                                                  row.get('description'),
-                                                                                  row.get('requirement')))
-            cur.execute("SELECT * FROM vacancy_table")
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+        with open('vacancy_json.json', 'r', encoding='utf-8') as file:
+            vacancies = json.load(file)
+            for vacancy in vacancies:
+                cur.execute(
+                    'INSERT INTO vacancy_table (company, job_title, link_to_vacancy, salary, description, '
+                    'requirement) VALUES (%s, %s, %s, %s, %s, %s)',
+                    (vacancy.get('company'), vacancy.get('job_title'), vacancy.get('link_to_vacancy'),
+                     vacancy.get('salary'), vacancy.get('description'), vacancy.get('requirement')))
+
+        cur.execute("SELECT * FROM vacancy_table")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
